26ae03abcccf55ed489769f410b7148518383fcd
CaioSouza021 <caio.souza.cs.2003@outlook.com.br>
2025-04-04 19:04:25 -0300

    projeto finalizado

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,7 +1,26 @@
 import S from "./doados.module.scss";
 import livros from "../../assets/livros.png";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function Doados() {
+  const [livro, setLivro] = useState([]);
+
+  const getLivros = async () => {
+    try {
+      const response = await axios.get(
+        "https://teste-deploy-bkai.onrender.com/livros"
+      );
+      setLivro(response.data);
+    } catch (error) {
+      console.error("Erro ao buscar livros:", error);
+    }
+  };
+
+  useEffect(() => {
+    getLivros();
+  });
+
   return (
     <section className={S.boxDoados}>
       <h2>Livros Doados</h2>
@@ -12,6 +31,14 @@ export default function Doados() {
           <p>Susanne Andrade</p>
           <p>Ficção</p>
         </article>
+        {livro.map((item) => (
+          <article key={item.id}>
+            <img src={item.imagem_url} alt="" />
+            <h3>{item.titulo}</h3>
+            <p>{item.categoria}</p>
+            <p>{item.autor}</p>
+          </article>
+        ))}
       </section>
     </section>
   );
